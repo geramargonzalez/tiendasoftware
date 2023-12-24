1db9e250cc6cff52777e243f1d92a52e17b1bd20
Gerardo Gonzalez <gerardo.gonzalez@techfino.com>
2023-12-24 14:27:03 -0300

    Text add

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import CartWidget from './components/CardWidget/CartWidget';
 function App() {
   return (
     <>
-    <ItemListContainer saludo='Hello there' despedida='Goodbye BABY'>
+    <ItemListContainer saludo='Hello there'>
   
     </ItemListContainer>
 
-    <CartWidget bodyCart='This is test'></CartWidget>
+    <CartWidget bodyCart=''></CartWidget>
 
     </>
   );
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -4,11 +4,12 @@ function ItemListContainer({saludo, despedida}) {
 
     return (
         <div>
-            <div>
-                {saludo}
+            <div class="welcome-text">
+            <p>Bienvenido a e-commerce de Gerardo Gonzalez</p>
             </div>
-            <div>
-                {despedida}
+            <div class="lorem-ipsum">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit varius mauris, vel interdum lorem consequat at. Donec luctus lectus in lorem tincidunt, vitae pharetra odio tristique.</p>
+                <p>Nullam ultrices ex vel justo molestie condimentum. Duis nec magna nec nulla dictum tincidunt. Aliquam vel augue vitae ligula eleifend ultricies a nec ligula.</p>
             </div>
         </div>
         )
